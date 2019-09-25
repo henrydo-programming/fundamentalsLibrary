8cb5225fb209e5e1228feea7d2ef3a46bf3eef39
Henry Do <46805405+henrydo-programming@users.noreply.github.com>
2019-09-25 02:30:53 -0700

    Update Create3x3Array.cpp

--- Create3x3Array.cpp
+++ Create3x3Array.cpp
@@ -4,20 +4,9 @@
 using namespace std;
 
 //generates a 3x3 array
-
-const int NUM_COLS = 3;									// Number of columns
-const int NUM_ROWS = 3;									// Number of rows
+const int NUM_COLS = 3;							// Number of columns
+const int NUM_ROWS = 3;							// Number of rows
 void showArray(const int[][NUM_COLS], int);
-
-int main()
-{
-	int table[NUM_ROWS][NUM_COLS] = { {2, 1, 1},
-										{1, 1, 0},
-										{0, 1, 1} };  		// 2D array with 3 rows & 3 columns
-	showArray(table, NUM_ROWS);
-
-	return 0;
-}
 
 // declares function showArray()
 void showArray(int const array[][NUM_COLS], int numRows)		//declares void type function that takes two arguments
@@ -30,4 +19,15 @@ void showArray(int const array[][NUM_COLS], int numRows)		//declares void type f
 		}
 		cout << endl;
 	}
+}
+
+//This is where the program starts.
+int main()
+{
+	int table[NUM_ROWS][NUM_COLS] = { 	{2, 1, 1},
+						{1, 1, 0},
+						{0, 1, 1} };  		// 2D array with 3 rows & 3 columns
+	showArray(table, NUM_ROWS);
+
+	return 0;
 }
