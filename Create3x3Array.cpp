96f17511b4d08557c6b9df9d35de3309363c4bef
Henry Do <46805405+henrydo-programming@users.noreply.github.com>
2019-09-25 02:28:08 -0700

    Add files via upload
    This program is the first of a series of "fundamental algorithms in my introduction to C++ computer science course.  
    It is the first step of a program to check vertical and horizontal neighbors of elements within an array for a condition.
    step 1: produce array
    step 2: check for neighbors 
    step 3: check neighbors for specific condition

---
+++ Create3x3Array.cpp
@@ -0,0 +1,33 @@
+//Neighbour-Finding in a Two-Dimensional Array
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//generates a 3x3 array
+
+const int NUM_COLS = 3;									// Number of columns
+const int NUM_ROWS = 3;									// Number of rows
+void showArray(const int[][NUM_COLS], int);
+
+int main()
+{
+	int table[NUM_ROWS][NUM_COLS] = { {2, 1, 1},
+										{1, 1, 0},
+										{0, 1, 1} };  		// 2D array with 3 rows & 3 columns
+	showArray(table, NUM_ROWS);
+
+	return 0;
+}
+
+// declares function showArray()
+void showArray(int const array[][NUM_COLS], int numRows)		//declares void type function that takes two arguments
+{
+	for (int row = 0; row < numRows; row++)						//loop counter for rows index
+	{
+		for (int col = 0; col < NUM_COLS; col++)				//loop counter for column index
+		{
+			cout << setw(5) << array[row][col] << " ";
+		}
+		cout << endl;
+	}
+}
